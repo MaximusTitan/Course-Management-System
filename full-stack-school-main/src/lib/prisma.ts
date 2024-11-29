7e439f884b0b779a0f45f157a9184bc1147318a4
MaximusTitan <igebra.genai3@gmail.com>
2024-11-29 15:47:21 +0530

    Update prisma.ts

--- full-stack-school-main/src/lib/prisma.ts
+++ full-stack-school-main/src/lib/prisma.ts
@@ -1,15 +1,16 @@
-// lib/prisma.js
-import { PrismaClient } from '@prisma/client';
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
-let prisma;
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  // Prevent re-creating the PrismaClient during hot-reloading in development
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = (global as any).prisma;
 }
 
 export default prisma;
